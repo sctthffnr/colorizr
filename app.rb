19bd787d856a569f62eb94e22d7751299c90e5a8
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-01-30 12:32:21 -0500

    Initial commit. Created methods to show all available colors and to print sample output demonstrating all colors.

---
+++ app.rb
@@ -0,0 +1,6 @@
+require_relative 'lib/colorizr'
+
+puts String.colors
+String.create_colors
+String.create_sample_colors
+puts String.sample_colors
---
+++ lib/colorizr.rb
@@ -0,0 +1,41 @@
+class String
+@@color_map = { black: 30,
+              red: 31,
+              green: 32,
+              yellow: 33,
+              blue: 34,
+              light_grey: 37,
+              light_blue: 94,
+              pink: 35 ,
+              white: 97}
+
+  def self.colors
+    @@color_map.keys
+  end
+
+  def self.create_colors
+    @@color_map.each do |color, code|
+      class_eval(%Q{
+        def #{color}
+          "\e[#{code}m\#{self}\e[0m"
+        end
+        })
+    end
+  end
+
+  def self.create_sample_colors
+    @@color_map.each_key do |color|
+      instance_eval(%Q{
+        def sample_#{color}
+          "This is " + "#{color}".#{color}
+        end
+        })
+    end
+  end
+
+  def self.sample_colors
+    @@color_map.each_key do |color|
+      puts eval("sample_#{color}")
+    end
+  end
+end
